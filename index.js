f3d449fcced89b615c3c9aeb280b26fbc3b2e8b0
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2023-09-10 01:00:06 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById('text').textContent = "This is really cool!"
+
+  // or using quertSelector
+  // document.querySelector('p').textContent = "This is really cool!"
+});
